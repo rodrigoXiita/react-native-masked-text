57bc4755f0b9df524b755d24700b87f8f88dbbfd
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2020-12-08 10:32:11 -0300

    .

---
+++ __tests__/mask/boleto.mask.test.js
@@ -0,0 +1,32 @@
+import {BoletoMask} from "../../lib/masks";
+
+test('getType results boleto', () => {
+    var expected = 'boleto'
+    var received = BoletoMask.getType()
+
+    expect(received).toBe(expected)
+})
+test('34191790010104351004791020150008684630026000 results 34191.79001 01043.510047 91020.150008 6 84630026000', () => {
+    var mask = new BoletoMask()
+    var expected = '34191.79001 01043.510047 91020.150008 6 84630026000'
+    var received = mask.getValue('34191790010104351004791020150008684630026000')
+
+    expect(received).toBe(expected)
+})
+
+test('341917900101043510047910201500086 results 34191.79001 01043.510047 91020.150008 6', () => {
+    var mask = new BoletoMask()
+    var expected = '34191.79001 01043.510047 91020.150008 6'
+    var received = mask.getValue('341917900101043510047910201500086')
+
+    expect(received).toBe(expected)
+})
+
+
+test('341917900101043510047 results 34191.79001 01043.510047', () => {
+    var mask = new BoletoMask()
+    var expected = '34191.79001 01043.510047'
+    var received = mask.getValue('341917900101043510047')
+
+    expect(received).toBe(expected)
+})
